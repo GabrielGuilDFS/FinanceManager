da68e905a76f9dbddd002fca6ec9bb42d596cf31
Guilherme G <guigabriel2811914@gmail.com>
2025-12-23 19:50:35 -0300

    Fix pasta backend: remove referencia submodule e localiza normalmente as pastas corretas

---
+++ backend/FinanceManager.Api/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceManager.Api.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
