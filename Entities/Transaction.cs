25d2b8525023eb725bb174d99dd8908b9861f0ca
Guilherme G <guigabriel2811914@gmail.com>
2025-12-23 19:07:07 -0300

    Criacão inicial do projeto + Criacão inicial do bbanco de dados

---
+++ Entities/Transaction.cs
@@ -0,0 +1,17 @@
+﻿namespace FinanceManager.Api.Entities
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; } 
+        public Decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Type {  get; set; }
+        public string Description { get; set; }
+
+        public Guid CategoryId { get; set; }
+        public Category category { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
